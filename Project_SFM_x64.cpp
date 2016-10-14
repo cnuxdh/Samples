18402834a92e15a91839053dff36f9daf0122bdc
cnuxdh <6121@cnu.edu.cn>
2016-10-14 16:53:06 +0800

    test my algorithm to generate tracks

--- Project_SFM_x64.cpp
+++ Project_SFM_x64.cpp
@@ -24,29 +24,35 @@ int main(int argc, char** argv[])
 
 	
 	//3.generating the tracks
-	/* test
-	imgFeatures.resize(3);
-	imgFeatures[0].featPts.resize(6);
-	imgFeatures[1].featPts.resize(6);
-	imgFeatures[2].featPts.resize(6);
-	char* matchfile = "c:\\temp\\match_file.txt";
+	//test
+	imgFeatures.resize(4);
+	int nFeat = 1000;
+	imgFeatures[0].featPts.resize(nFeat);
+	imgFeatures[1].featPts.resize(nFeat);
+	imgFeatures[2].featPts.resize(nFeat);
+	imgFeatures[3].featPts.resize(nFeat);
+
+	//char* matchfile = "c:\\temp\\match_file.txt";
+	char* matchfile = "c:\\sfm\\match_init.txt";
 	LoadMatchFile(matchfile, matchRes);
-	*/
+	
 
 	vector<TrackInfo> tracks; 
 	CGenerateTracksBase* pGenerateTrack = new CFastGenerateTrack();
 	pGenerateTrack->GenerateTracks(imgFeatures, matchRes, tracks);
 	
+	FILE* fp = fopen("c:\\temp\\mytrack.txt", "w");
+	fprintf(fp, "%d \n", tracks.size());
 	for(int i=0; i<tracks.size(); i++)
 	{
-		printf("track: %d \n", i);
+		fprintf(fp, "%d  ", tracks[i].views.size());
 		for(int j=0; j<tracks[i].views.size(); j++)
 		{
-			printf(" %d-%d  ", tracks[i].views[j].first, tracks[i].views[j].second );
+			fprintf(fp, " %d %d ", tracks[i].views[j].first, tracks[i].views[j].second );
 		}
-		printf("\n");
+		fprintf(fp, "\n");
 	}
-
+	fclose(fp);
 
 	return 0;
 }
